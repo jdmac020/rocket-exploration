441c284e8253a9f36ee72fa99d7d3de96c8bdedb
jdmac020 <john.macgregor@gmail.com>
2021-08-28 11:55:12 -0400

    Refactor and organize

---
+++ tests/basic_test.rs
@@ -0,0 +1,12 @@
+
+use rocket_explore::rocket_builder;
+use rocket::local::Client;
+use rocket::http::Status;
+
+#[test]
+fn echo_test() {
+    let client = Client::new(rocket_builder()).expect("Valid Rocket instance");
+    let mut response = client.get("/echo/test_echo").dispatch();
+    assert_eq!(response.status(), Status::Ok);
+    assert_eq!(response.body_string(), Some("test_echo".into()));
+}
