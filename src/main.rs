8d0c9102c58fbe516bbb47540ba881026d439633
jdmac020 <john.macgregor@gmail.com>
2021-08-28 10:46:20 -0400

    Serve a static file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use std::path::{Path, PathBuf};
+
 #[macro_use] use rocket::*;
+use rocket_contrib::serve::StaticFiles;
 
 #[get("/echo/<echo>")]
 fn echo_fn(echo: String) -> String {
@@ -8,5 +11,8 @@ fn echo_fn(echo: String) -> String {
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![echo_fn]).launch();
+    rocket::ignite()
+    .mount("/", routes![echo_fn])
+    .mount("/files", StaticFiles::from("static/"))
+    .launch();
 }
